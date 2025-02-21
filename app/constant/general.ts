2c841ee750b55e03292b984d20cc7c0adca6fb8f
bobi.rival <bahmadrival@gmail.com>
2025-02-21 15:01:08 +0700

    Fixing login

---
+++ app/constant/general.ts
@@ -0,0 +1,1 @@
+export const PAYROLL_LOGO: string = "/images/payroll-logo.png";
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,20 +5,34 @@ import { useState } from "react";
 import InputLabel from "../component/input/input-label";
 import ButtonLoading from "../component/button/button-loading";
 import ButtonIcon from "../component/button/button-icon";
+import { LoginRequest } from "../dto/request/login-request";
+
+const USERNAME: string = "username";
+const PASSWORD: string = "password";
 
 export default function Login() {
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const submitLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    const submitLogin = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
         try {
             setIsLoading(true);
             e.preventDefault();
-            console.log("Hello");
+
+            const formData = new FormData(e.currentTarget);
+            const request = buildLoginRequest(formData);
+            console.log(request);
         } catch (error) {
             console.error(error);
         } finally {
-            // setIsLoading(false);
+            setIsLoading(false);
         }
+    };
+
+    const buildLoginRequest = (formData: FormData): LoginRequest => {
+        return {
+            username: (formData.get(USERNAME) as string) ?? "",
+            password: (formData.get(PASSWORD) as string) ?? "",
+        };
     };
 
     return (
@@ -28,13 +42,12 @@ export default function Login() {
                     <div className="p-6 md:p-8">
                         <div className="flex justify-center gap-3">
                             <Image key={"payroll-logo"} className="w-auto h-9" src={"/images/payroll-logo.png"} alt="Payroll Logo" width={100} height={36} priority unoptimized />
-                            {/* <img src="/images/payroll-logo.png" alt="Payroll Logo" className="w-auto h-9" /> */}
                             <span className="self-center text-primary-700 text-xl font-semibold whitespace-nowrap">Applikasi Penggajian</span>
                         </div>
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-center text-gray-900 md:text-2xl mb-6">Masuk ke akun anda</h1>
                         <form onSubmit={submitLogin} className="flex flex-col gap-4">
-                            <InputLabel label="Username" name={"username"} type="text" isRequired={true} />
-                            <InputLabel label="Password" name={"password"} type="password" placeHolder="••••••••" isRequired={true} />
+                            <InputLabel label="Username" name={USERNAME} type="text" isRequired={true} />
+                            <InputLabel label="Password" name={PASSWORD} type="password" placeHolder="••••••••" isRequired={true} />
                             {isLoading ? <ButtonLoading text="Proses Login..." className="mt-2" /> : <ButtonIcon type="submit" icon="fa-solid fa-right-to-bracket" text="Login" className="mt-2 py-2.5" />}
                         </form>
                     </div>
