242cb7a9a5ed054a8661262b0d372b7725278b34
bobi.rival <bahmadrival@gmail.com>
2025-02-26 14:26:40 +0700

    create fetch api employee

--- app/api/user.ts
+++ app/api/user.ts
@@ -1,3 +1,5 @@
+"use client"
+
 import { BE_USER } from "../constant/endpoint-be";
 import { Pagination } from "../dto/dto/pagination";
 import { Search } from "../dto/dto/search";
--- app/constant/endpoint-be.ts
+++ app/constant/endpoint-be.ts
@@ -7,3 +7,4 @@ export const BE_ALLOWANCE_TYPE: string = BE_BASE + "/allowance-types";
 export const BE_POSITION: string = BE_BASE + "/positions";
 export const BE_BASIC_SALARY: string = BE_BASE + "/basic-salaries";
 export const BE_ALLOWANCE: string = BE_BASE + "/allowances";
+export const BE_EMPLOYEE: string = BE_BASE + "/employees";
